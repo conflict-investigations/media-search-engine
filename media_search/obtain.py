6f2922241976ec621f1941ae64b4daf0d49221c6
Anonymous <none@none.none>
2023-01-11 01:51:51 +0100

    obtain: Add and improve type hints

--- media_search/obtain.py
+++ media_search/obtain.py
@@ -1,7 +1,7 @@
 import json
 import os
 from dataclasses import fields
-from typing import Any, Dict, List
+from typing import Any, List
 
 from geo_extractor.constants import RAW_DATA_FILENAMES, SOURCE_NAMES
 from geo_extractor import DOWNLOADERS, EXTRACTORS
@@ -24,7 +24,7 @@ def save_source(sourcename: str) -> None:
     with open(get_file(sourcename), 'w') as f:
         json.dump(data, f, ensure_ascii=False)
 
-def build_mapping(events: Any) -> dict[str, List[dict]]:
+def build_mapping(events: List[Any]) -> dict[str, List[dict]]:
     url_to_data_mapping = {}  # type: dict[str, List[dict]]
     for e in events:
         for link in e.links:
@@ -52,7 +52,7 @@ def build_mapping(events: Any) -> dict[str, List[dict]]:
     # TODO: Special case for reukraine data
     # If "magic" JSON file is not present the source will not be used
     try:
-        reukraine = load_reukraine()
+        reukraine: dict[str, Any] = load_reukraine()
         for key in reukraine.keys():
             if key not in url_to_data_mapping:
                 url_to_data_mapping[key] = []
@@ -63,32 +63,32 @@ def build_mapping(events: Any) -> dict[str, List[dict]]:
     return url_to_data_mapping
 
 # TODO: Handle this in geo_extractor
-def load_reukraine() -> Dict[str, Any]:
-    REUKRAINE_FILENAME = 'reukraine-full.json'
-    filename = os.path.join(CONFIG.DATA_FOLDER, REUKRAINE_FILENAME)
-    with open(filename, 'r') as f:
-        data = json.loads(f.read())
+def load_reukraine() -> dict[str, Any]:
+    REUKRAINE_FILENAME: str = 'reukraine-full.json'
+    filepath = os.path.join(CONFIG.DATA_FOLDER, REUKRAINE_FILENAME)
+    with open(filepath, 'r') as f:
+        data: Any = json.loads(f.read())
 
-    processed = {}  # type: dict[str, List[dict[str, Any]]]
+    processed: dict[str, List[dict[str, Any]]] = {}
     for item in data:
-        link = item.get('link')
+        link: str = item.get('link', '')
         # Some sources contain non-url sources
         if not link.startswith('http'):
             continue
         sanitized = normalize_and_sanitize(link)
-        loc = dict(
+        loc: dict[str, Any] = dict(
             latitude=item.get('latitude'),
             longitude=item.get('longitude'),
             place_desc=" - ".join(filter(None, (item.get('address'),
                                                 item.get('region')))),
         )
-        date = ''
+        date: str = ''
         if (d := item.get('happend')):  # 'happend', not a typo
             date = f'Date: {d}'
-        description = " - ".join(filter(None,
-                                        (date,
-                                         item.get('type'),
-                                         item.get('infotxt'))))
+        description: str = " - ".join(filter(None,
+                                             (date,
+                                              item.get('type'),
+                                              item.get('infotxt'))))
         if sanitized not in processed:
             processed[sanitized] = []
 
@@ -108,7 +108,7 @@ def ensure_data_dir() -> None:
 def download_data() -> None:
     ensure_data_dir()
     for _field in fields(SOURCE_NAMES):
-        sourcename = _field.name
+        sourcename: str = _field.name
         # Skip DefMon3 shellings, see below
         if sourcename == SOURCE_NAMES.DEFMON:
             continue
@@ -116,9 +116,9 @@ def download_data() -> None:
     print('  Download finished')
 
 def load_and_generate_mapping() -> dict[str, List[dict[str, Any]]]:
-    events = []
+    events: List[Any] = []
     for _field in fields(SOURCE_NAMES):
-        sourcename = _field.name  # type: str
+        sourcename: str = _field.name
         # We're not interested in DefMon3 shellings etc. since they mostly
         # contain no useful links
         if sourcename == 'DEFMON':
@@ -129,5 +129,5 @@ def load_and_generate_mapping() -> dict[str, List[dict[str, Any]]]:
             print(f"{sourcename} file missing")
             save_source(sourcename)
             events.extend(load_source(sourcename))
-    processed = build_mapping(events)  # type: dict[str, List[dict[str, Any]]]
+    processed: dict[str, List[dict[str, Any]]] = build_mapping(events)
     return processed
